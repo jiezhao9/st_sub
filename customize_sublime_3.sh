308aa175385a02fe09dfbdf78342e937d6f6755c
sybertoto <xst0001@gmail.com>
2015-12-29 21:27:39 +0800

    Set APPDATA for Linux

--- customize_sublime_3.sh
+++ customize_sublime_3.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -x #echo on
 
-SUBL_PATH="$APPDATA/Sublime Text 3"
-PACKAGE_PATH="$SUBL_PATH/Installed Packages"
+OS=$(uname)
 
+if [ $OS == "Linux" ]; then
+	APPDATA="~/.config"
+	SUBL_PATH="$APPDATA/Sublime-Text-3"
+else
+	SUBL_PATH="$APPDATA/Sublime Text 3"
+fi
+
+PACKAGE_PATH="$SUBL_PATH/Installed Packages"
 SETTING_FILE="Package Control.sublime-settings"
 SETTING_PATH="$SUBL_PATH/Packages/User"
 
